09c2738b1a395d9419f72919dc33dcf22d986290
Emmanuel Agbo <link4emmy@gmail.com>
2024-10-23 21:00:51 +0100

    creating GroupList

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,13 @@
+function ListGroup() {
+  return (
+    <ul ClassName="list-group">
+      <li ClassName="list-group-item">An item</li>
+      <li ClassName="list-group-item">A second item</li>
+      <li ClassName="list-group-item">A third item</li>
+      <li ClassName="list-group-item">A fourth item</li>
+      <li ClassName="list-group-item">And a fifth one</li>
+    </ul>
+  );
+}
+
+export default ListGroup;
