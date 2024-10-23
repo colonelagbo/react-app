930a1526f7ffb48068a78263c317d2e93be79fa6
Emmanuel Agbo <link4emmy@gmail.com>
2024-10-23 22:04:14 +0100

    conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,13 @@
 import { fragment } from "react";
 
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "Lodon", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "Lodon", "Paris"];
+  items = [];
 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
