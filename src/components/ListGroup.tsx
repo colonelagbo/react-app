77880a02ad638c49e69b4e8e555272d448d9b2df
Emmanuel Agbo <link4emmy@gmail.com>
2024-10-23 21:10:42 +0100

    Fragment

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,17 @@
+import { fragment } from "react";
+
 function ListGroup() {
   return (
-    <ul ClassName="list-group">
-      <li ClassName="list-group-item">An item</li>
-      <li ClassName="list-group-item">A second item</li>
-      <li ClassName="list-group-item">A third item</li>
-      <li ClassName="list-group-item">A fourth item</li>
-      <li ClassName="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List</h1>
+      <ul ClassName="list-group">
+        <li ClassName="list-group-item">An item</li>
+        <li ClassName="list-group-item">A second item</li>
+        <li ClassName="list-group-item">A third item</li>
+        <li ClassName="list-group-item">A fourth item</li>
+        <li ClassName="list-group-item">And a fifth one</li>
+      </ul>
+    </>
   );
 }
 
