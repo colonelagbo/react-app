7df61e825255d39ba15c1fd5634f23fffd2f05ff
Emmanuel Agbo <link4emmy@gmail.com>
2024-11-01 16:45:09 +0100

    Passing childrem to a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "Lodon", "Paris"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities "
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>Hello World</Alert>
     </div>
   );
 }
